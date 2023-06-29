4816fc3228b439cf3fad703134a1a4c93d1832f8
Maxime Monnier <maximemonnier39@gmail.com>
2023-06-29 18:19:43 +0400

    mainpage ok

---
+++ src/components/Article.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Blog = () => {
+  return (
+    <div>
+      <NavLink className="article-container">
+        <p>toto</p>
+      </NavLink>
+    </div>
+  );
+};
+
+export default Blog;
--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,9 +1,26 @@
 import React from "react";
+import LogoComponent from "../components/LogoComponent";
+import PowerButton from "../components/PowerButton";
+import SocialCons from "../components/SocialCons";
+import Article from "../components/Article";
+import { Blogs } from "../data/BlogData";
 
 const BlogPage = () => {
+  console.log(Blogs);
   return (
-    <div>
-      <div>BlogPage Component/page</div>
+    <div className="container-blog-main">
+      <div className="container-blog-sec">
+        <LogoComponent />
+        <PowerButton />
+        <SocialCons />
+        <div className="center-blog">
+          <div className="grid-blog">
+            {Blogs.map((article) => {
+              return <Article key={article.id} article={article} />;
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
