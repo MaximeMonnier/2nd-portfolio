048582280ac4ec62d62c9bf3d0d72618e81748ca
Maxime Monnier <maximemonnier39@gmail.com>
2023-08-24 20:58:13 +0200

    work page animatiojn start

--- src/pages/WorkPage.js
+++ src/pages/WorkPage.js
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PowerButton from "../components/PowerButton";
 import SocialConsD from "../components/SocialConsD";
 import LogoComponentD from "../components/LogoComponentD";
 import { Work } from "../data/WorkData";
 import Card from "../components/Card";
 
-const AboutPage = () => {
+const WorkPage = () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    let element = ref.current;
+
+    window.addEventListener("scroll", (e) => {
+      const scrollY = window.scrollY; // Obtient la valeur du défilement vertical
+      const scrollX = window.scrollX; // Obtient la valeur du défilement horizontal
+
+      console.log("Scroll Y:", scrollY);
+      console.log("Scroll X:", scrollX);
+    });
+
+    const rotate = () => {
+      element.style.transform = `translateX(${-window.scrollY}px)`;
+    };
+
+    window.addEventListener("scroll", rotate);
+
+    return () => window.removeEventListener("scroll", rotate);
+  }, []);
+
   return (
     <div>
-      <div className="container-work">
+      <div className="container-work" ref={ref}>
         <SocialConsD />
         <PowerButton />
         <LogoComponentD />
-        <div className="container-card">
+        <div className="work-back">
+          <p>WORK</p>
+        </div>
+        <ul className="container-card">
           {Work.map((d) => {
             return <Card key={d.id} data={d} />;
           })}
-        </div>
+        </ul>
       </div>
     </div>
   );
 };
 
-export default AboutPage;
+export default WorkPage;
